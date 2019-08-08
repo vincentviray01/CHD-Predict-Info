11e070d2f108a0ec99f211538eaba98e74d86493
unknown <vincentviray01@gmail.com>
2019-08-07 23:10:34 -0500

    Same stuff tried to changed and changed to debug mode

--- chd/settings.py
+++ chd/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+# DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+DEBUG = True
 
 ALLOWED_HOSTS = ['chdpredictorinfo.herokuapp.com']
 
