7259aadaa3a57d61845310f9ede7ef32684761a5
unknown <vincentviray01@gmail.com>
2019-08-07 22:48:40 -0500

    Updated Settings

--- chd/settings.py
+++ chd/settings.py
@@ -16,16 +16,19 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7#=$sb3am!98b$-uzpwwkzrc28#tvfq6+5qigg89dyhs70#jjd'
+# SECRET_KEY = '7#=$sb3am!98b$-uzpwwkzrc28#tvfq6+5qigg89dyhs70#jjd'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['chdpredictorinfo.herokuapp.com']
 
 
 # Application definition
